12db5481af6ce88e4b03c02426f04cbfa70e2ab8
crazycatwork <127667032+crazycatwork@users.noreply.github.com>
2023-09-14 09:48:54 -0400

    troubleshooting

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+from stories import story
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    prompts = story.prompts
+    return render_template("prompts.html", prompts=prompts)
+
+@app.route('/story')
+def story():
+    text = story.generate(request.args)
+    return render_template("story.html", text=text)
